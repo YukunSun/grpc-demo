1423d951b9b0caed88dc76bcd167735627a4a249
sunyukun <af>
2018-10-18 20:02:02 +0800

    init

---
+++ grpc-client/src/main/java/net/coderdaily/grpcclient/helloworld/HelloWorldClient.java
@@ -0,0 +1,11 @@
+package net.coderdaily.grpcclient.helloworld;
+
+/**
+ * Author: sunyukun@xueqiu.com
+ * Time: 2018/9/30 下午2:09
+ * Desc:
+ */
+public class HelloWorldClient {
+    //private
+
+}
---
+++ grpc-proto/src/main/java/net/coderdaily/grpcproto/GrpcProtoApplication.java
@@ -0,0 +1,12 @@
+package net.coderdaily.grpcproto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcProtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcProtoApplication.class, args);
+    }
+}
